135df5a0600baf1162ef0eefe0a408d9e6e15b55
Shubham Muneshwar <shubham@Shubhams-MacBook-Air.local>
2018-09-25 05:11:36 +0530

    Edit Note - 2

--- Frontend/src/containers/note/edit_form.js
+++ Frontend/src/containers/note/edit_form.js
@@ -10,7 +10,7 @@ class EditForm extends Component{
 		const {data} = this.props.data;
 		//console.log(data.id);
 		this.props.editPost(formValue,data.id,()=>{
-			this.props.history.push(`/view_note/${data.id}`);
+			this.props.history.push(`/edit_note/${data.id}`);
 		});
 	}
 	componentDidMount() {
@@ -24,18 +24,32 @@ class EditForm extends Component{
 		const {handleSubmit} = this.props;
 		const {data} = this.props.data;
 		return(
-			<div className="note-detail-container">
-				<div className="note-detail-top-row">
-				 	<button className="btn btn-primary save-note" type="submit">Save</button>
-					<div className="icons">
-						<i className="fa fa-external-link" aria-hidden="true"></i>
-						<i className="fa fa-share-square-o" aria-hidden="true"></i>
-						<i className="fa fa-trash" aria-hidden="true"></i>
+			<form onSubmit={handleSubmit(this.formSubmit.bind(this))}>
+				<div className="note-detail-container">
+					<div className="note-detail-top-row">
+					 	<button className="btn btn-primary save-note" type="submit">Save</button>
+						<div className="icons">
+							<i className="fa fa-external-link" aria-hidden="true"></i>
+							<i className="fa fa-share-square-o" aria-hidden="true"></i>
+							<i className="fa fa-trash" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div className="note-title">
+						<Field component={renderInput}
+							label="Title"
+							type = "text"
+							name = "title"
+						/>
+					</div>
+					<div className="note-content">
+						<Field component={renderInput}
+						label="Content"
+						type = "text"
+						name = "content"
+						/>
 					</div>
 				</div>
-				<div className="note-detail-body">
-				</div>
-			</div>
+		</form>
 		);
 	}
 }
